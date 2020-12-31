4a039879210bc36c133e4fb38bf2fecf8ff58f8d
Doug Bromley <doug@tintophat.com>
2020-12-31 16:01:15 +0000

    Add comment of my scribbling on notepad

--- factorial.scm
+++ factorial.scm
@@ -2,6 +2,7 @@
 ;;; Built using recursion after learnign some basic Scheme
 ;;; Copyright Doug Bromley <doug@tintophat.com>
 ;;; BSD License 3 clause like everything else
+;;; y = (x * (x - 1)) * ((x - 1) * ((x - 1) - 1)) recursive hence why I thought it a good fit for Scheme
 
 (define (fact x)
   (if (= x 1)
